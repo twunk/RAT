6f014bb0869368e4b4942266513c777e55bba038
Twunk <gal.peter92@gmail.com>
2020-11-27 15:40:35 +0100

    set .net core version to 2.1.1, added kestrel configuration, removed unused pages

--- RATWeb/RATWeb/Program.cs
+++ RATWeb/RATWeb/Program.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server.Kestrel;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
 
 namespace RATWeb
 {
@@ -19,6 +23,51 @@ namespace RATWeb
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                // Old config load
+                //.ConfigureServices((context, services) =>
+                //{
+                //    services.Configure<KestrelServerOptions>(
+                //        context.Configuration.GetSection("Kestrel"));
+                //})
+                .UseStartup<Startup>()
+                .UseKestrel(serverOptions =>
+                {
+                    // Set Kestrel options
+                    serverOptions.Limits.KeepAliveTimeout = TimeSpan.FromSeconds(20);
+                    serverOptions.Limits.MaxConcurrentConnections = 64;
+                    serverOptions.Limits.MaxConcurrentUpgradedConnections = 64;
+                    serverOptions.Limits.MaxRequestBodySize = 2 * 1024;
+
+                    serverOptions.Limits.MinRequestBodyDataRate =
+                        new MinDataRate(
+                            bytesPerSecond: 124, 
+                            gracePeriod: TimeSpan.FromSeconds(5)
+                        );
+
+                    serverOptions.Limits.MinResponseDataRate =
+                        new MinDataRate(
+                            bytesPerSecond: 124, 
+                            gracePeriod: TimeSpan.FromSeconds(5)
+                        );
+
+                    serverOptions.Limits.RequestHeadersTimeout = TimeSpan.FromSeconds(28);
+
+                    serverOptions.AllowSynchronousIO = false;
+
+                    // Configure endpoint
+                    serverOptions.ConfigureEndpointDefaults(listenOptions =>
+                    {
+                        // listenOptions.ServerCertificate = certificate;
+
+
+                    });
+
+                    serverOptions.Listen(IPAddress.Loopback, 16420, listenOptions =>
+                    {
+                        listenOptions.UseConnectionLogging();
+                        // listenOptions.UseHttps("testCert.pfx", "testPassword");
+                    });
+
+                });
     }
 }
